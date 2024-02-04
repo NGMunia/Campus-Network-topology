41be9c472370ca27445337e6174cf015561bf2dc
NGMunia <anthony.ngure@yahoo.com>
2024-02-04 12:51:28 +0300

    Update Configure.py (NTP)

--- Configure.py
+++ Configure.py
@@ -8,27 +8,27 @@ from Network.Devices import Area_0, Firewalls, Edge_Routers, Spokes, Switches
 
 # Configuring Netflow on Branch routers:
 Template_dir =input('Jinja Templates Directory filepath: ')
-# rp(f'[bold cyan]----------Configuring NetFlow----------[/cyan]')
-# for devices in Spokes.values():
-#     c = ConnectHandler(**devices)
-#     c.enable()
-#     host  = c.send_command('show version', use_textfsm=True)[0]['hostname']
+rp(f'[bold cyan]----------Configuring NetFlow----------[/cyan]')
+for devices in Spokes.values():
+    c = ConnectHandler(**devices)
+    c.enable()
+    host  = c.send_command('show version', use_textfsm=True)[0]['hostname']
 
-#     server = input('IP address of the Flow Collector: ')
-#     interface = input(f'{host} Netflow source interface: ')
-#     Port = input(f'{host} Netflow UDP-Port: ')
+    server = input('IP address of the Flow Collector: ')
+    interface = input(f'{host} Netflow source interface: ')
+    Port = input(f'{host} Netflow UDP-Port: ')
 
-#     data = {
-#             'Flow_Interface': interface,
-#             'UDP_Port': Port,
-#             'Server_IP': server  
-#            }
-#     env = Environment(loader=FileSystemLoader(Template_dir))
-#     template = env.get_template('NetFlow.j2')
-#     commands = template.render(data)
-#     rp(c.send_config_set(commands.splitlines()),'\n')
-#     c.save_config()
-#     c.disconnect()
+    data = {
+            'Flow_Interface': interface,
+            'UDP_Port': Port,
+            'Server_IP': server  
+           }
+    env = Environment(loader=FileSystemLoader(Template_dir))
+    template = env.get_template('NetFlow.j2')
+    commands = template.render(data)
+    rp(c.send_config_set(commands.splitlines()),'\n')
+    c.save_config()
+    c.disconnect()
 
 
 # Configuring NTP
