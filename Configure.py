a317d4debd2e6f262f9e27a2e2cdb2676069ed65
NGMunia <anthony.ngure@yahoo.com>
2024-02-11 17:58:07 +0300

    Update

--- Configure.py
+++ Configure.py
@@ -6,74 +6,74 @@ from jinja2 import FileSystemLoader, Environment
 from Network.Devices import Area_0, Firewalls, Edge_Routers, Spokes, Switches
 
 
-# # Jinja Templates Directory filepath:
-# Template_dir =input('Jinja Templates Directory filepath: ')
+# Jinja Templates Directory filepath:
+Template_dir =input('Jinja Templates Directory filepath: ')
 
 
-# # Configuring Netflow on Branch routers:
-# rp(f'[bold cyan]----------Configuring NetFlow----------[/cyan]')
-# for devices in Spokes.values():
-#     c = ConnectHandler(**devices)
-#     c.enable()
-#     host  = c.send_command('show version', use_textfsm=True)[0]['hostname']
+# Configuring Netflow on Branch routers:
+rp(f'[bold cyan]----------Configuring NetFlow----------[/cyan]')
+for devices in Spokes.values():
+    c = ConnectHandler(**devices)
+    c.enable()
+    host  = c.send_command('show version', use_textfsm=True)[0]['hostname']
 
-#     server = input('IP address of the Flow Collector: ')
-#     interface = input(f'{host} Netflow source interface: ')
-#     Port = input(f'{host} Netflow UDP-Port: ')
+    server = input('IP address of the Flow Collector: ')
+    interface = input(f'{host} Netflow source interface: ')
+    Port = input(f'{host} Netflow UDP-Port: ')
 
-#     data = {
-#             'Flow_Interface': interface,
-#             'UDP_Port': Port,
-#             'Server_IP': server  
-#            }
-#     env = Environment(loader=FileSystemLoader(Template_dir))
-#     template = env.get_template('NetFlow.j2')
-#     commands = template.render(data)
-#     rp(c.send_config_set(commands.splitlines()),'\n')
-#     c.save_config()
-#     c.disconnect()
-
-
-# # Configuring NTP
-# rp(f'\n[bold cyan]----------Configuring NTP on Network Devices---------[/bold cyan]')
-# for devices in chain(Area_0.values(), Firewalls.values(), Edge_Routers.values(),
-#                      Spokes.values(),Switches.values()):
-#     c = ConnectHandler(**devices)
-#     c.enable()
-#     host  = c.send_command('show version', use_textfsm=True)[0]['hostname']
-#     ntp = input(f'NTP server IP for host  {host}: ')
-#     source = input(f'{host} NTP source interface: ')
-
-#     data = {
-#             'ntp_server': ntp,
-#             'source_intf': source
-#            }
-#     env = Environment(loader=FileSystemLoader(Template_dir))
-#     template = env.get_template('NTP.j2')
-#     commands = template.render(data)
-#     rp(c.send_config_set(commands.splitlines()),'\n')
-#     c.save_config()
-#     c.disconnect()
+    data = {
+            'Flow_Interface': interface,
+            'UDP_Port': Port,
+            'Server_IP': server  
+           }
+    env = Environment(loader=FileSystemLoader(Template_dir))
+    template = env.get_template('NetFlow.j2')
+    commands = template.render(data)
+    rp(c.send_config_set(commands.splitlines()),'\n')
+    c.save_config()
+    c.disconnect()
 
 
-# #Configuring CoPP
-# rp(f'\n[bold cyan]----------Configuring Edge Routers---------[/bold cyan]')
-# for devices in Edge_Routers.values():
-#     c = ConnectHandler(**devices)
-#     c.enable()
-#     host  = c.send_command('show version', use_textfsm=True)[0]['hostname']
-#     eigrp_enabled = input(f'Is EIGRP protocol Running on {host} (Y/N): ')
-#     ospf_enabled = input(f'Is OSPF protocol Running on {host} (Y/N): ')
-#     data = {
-#             'eigrp_enabled': eigrp_enabled,
-#             'ospf_enabled': ospf_enabled
-#            }
-#     env = Environment(loader=FileSystemLoader(Template_dir))
-#     template = env.get_template('CoPP.j2')
-#     commands = template.render(data)
-#     rp(c.send_config_set(commands.splitlines()),'\n')
-#     c.save_config()
-#     c.disconnect()
+# Configuring NTP
+rp(f'\n[bold cyan]----------Configuring NTP on Network Devices---------[/bold cyan]')
+for devices in chain(Area_0.values(), Firewalls.values(), Edge_Routers.values(),
+                     Spokes.values(),Switches.values()):
+    c = ConnectHandler(**devices)
+    c.enable()
+    host  = c.send_command('show version', use_textfsm=True)[0]['hostname']
+    ntp = input(f'NTP server IP for host  {host}: ')
+    source = input(f'{host} NTP source interface: ')
+
+    data = {
+            'ntp_server': ntp,
+            'source_intf': source
+           }
+    env = Environment(loader=FileSystemLoader(Template_dir))
+    template = env.get_template('NTP.j2')
+    commands = template.render(data)
+    rp(c.send_config_set(commands.splitlines()),'\n')
+    c.save_config()
+    c.disconnect()
+
+
+#Configuring CoPP
+rp(f'\n[bold cyan]----------Configuring Edge Routers---------[/bold cyan]')
+for devices in Edge_Routers.values():
+    c = ConnectHandler(**devices)
+    c.enable()
+    host  = c.send_command('show version', use_textfsm=True)[0]['hostname']
+    eigrp_enabled = input(f'Is EIGRP protocol Running on {host} (Y/N): ')
+    ospf_enabled = input(f'Is OSPF protocol Running on {host} (Y/N): ')
+    data = {
+            'eigrp_enabled': eigrp_enabled,
+            'ospf_enabled': ospf_enabled
+           }
+    env = Environment(loader=FileSystemLoader(Template_dir))
+    template = env.get_template('CoPP.j2')
+    commands = template.render(data)
+    rp(c.send_config_set(commands.splitlines()),'\n')
+    c.save_config()
+    c.disconnect()
 
 
 # Configure Syslog
